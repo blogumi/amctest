ba627a3136b8a11fa0804f95846d59c069d91802
blogumi <tj.blogumas@codeblogkc.com>
2018-08-14 21:58:23 -0500

    beta working

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-var Slackhook = require('slackhook');
-var slack = new Slackhook({
-    domain: 'amcconsumerappadmin',
-    token: '8niAg96MHDMHaNOQJTOwvnVz'
-});
-
+var moment = require('moment');
+const axios = require('axios');
 var express = require('express');
-// this example uses express web framework so we know what longer build times
-// do and how Dockerfile layer ordering matters. If you mess up Dockerfile ordering
-// you'll see long build times on every code change + build. If done correctly,
-// code changes should be only a few seconds to build locally due to build cache.
-
+const bodyParser = require('body-parser');
 var morgan = require('morgan');
-// morgan provides easy logging for express, and by default it logs to stdout
-// which is a best practice in Docker. Friends don't let friends code their apps to
-// do app logging to files in containers.
-
-// Appi
+const leanKitUrl = "https://amctheatres.leankit.com/kanban/api/board/351820956/AddCard/lane/693496503/position/0"
+// App
 var app = express();
+// create application/json parser
+var jsonParser = bodyParser.json()
 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(morgan('common'));
+
 
 app.get('/', function (req, res) {
 	var defaultResp = {
@@ -28,18 +22,49 @@ app.get('/', function (req, res) {
 });
 
 app.get('/healthz', function (req, res) {
-	// do app logic here to determine if app is truly healthy
-	// you should return 200 if healthy, and anything else will fail
-	// if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
 	var defaultResp = {
 		message: 'I am happy and healthy'
 	}
   res.send(JSON.stringify(defaultResp));
 });
 
-app.post('/chatops', function(req, res){
-	var hook = slack.respond(req.body);
-	res.json({text: 'Hi ' + hook.user_name, username: 'Dr. Nick'});
+app.post('/chatops', urlencodedParser, function(req, res){
+	console.log(req.body)
+	var responseUrl = req.body.response_url
+	var requestUser = req.body.user_name
+	var requestText = req.body.text
+	requestText = requestText.toLowerCase();
+
+	if(requestText === "help" || requestText === ""){
+		res.json({text: 'HELP: /request deployment [stage|prod] <appName> <details>', username: requestUser});
+	}else{
+		var regex = /deployment.(stage|prod)/
+		var result = regex.test(requestText);
+		if(result){
+			var date = moment()
+            date = moment().add(2, 'd');
+            var dateMsg = moment(date).format('MM') +'/'+ moment(date).format('DD') +'/'+ moment(date).format('YYYY')
+            var leanKitCard = {
+                "Title": 'Deployment Request: ' + requestText,
+                "Description": requestText + ' . Submitted Via ChatOps.',
+                "TypeId": 352181976,
+                "Priority": 1,
+                "Size": 2,
+                "IsBlocked": false,
+                "BlockReason": "",
+                "DueDate": dateMsg,
+                "ExternalSystemName": "Slack",
+                "ExternalSystemUrl": responseUrl,
+                "Tags": "deployment"
+            }
+            axios.post(leanKitUrl, leanKitCard, {auth: { username: 'ABlogumas@amctheatres.com', password: 'Splunker01!' }}).then(resp=>console.log(resp)).catch(error=>console.log(error))
+			res.json({text: 'Request Logged. Thanks for using AMCs ChatOps Chatbot!', username: requestUser});
+		}else{
+			res.json({text: 'Invalid Command. HELP: /request deployment [stage|prod] <appName> <details>', username: requestUser});
+		}
+	}
+	
+	
 });
 
 
